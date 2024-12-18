9ba0ec7d5377bdb387c36b6dbfc771b559f71795
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2024-12-18 10:43:24 +0530

    Update gtts_read_aloud.py
    cleanup

--- src/gtts_read_aloud.py
+++ src/gtts_read_aloud.py
@@ -55,31 +55,7 @@ def save_audio(text):
     ttsa.save('assistant.mp3')
     end_time = time.time()
     return end_time - st_time
-   
-text = '''
-    Good morning sir. The main updates today are as follows:
-    News:.
-        1;   Latest in the Middle East as US secretary of state meets with leaders in region.
-        2;   Meet the kids struggling to breathe in India’s choked capital.
-        3;   Buckingham Palace maid arrested after drunken brawl at work Christmas party.
-        4;   Indian Gukesh Dommaraju, 18, becomes the youngest ever chess world champion after Ding Liren blunder.
-        5;   Oldest human DNA reveals lost branch of the human family tree. 
-        6;   The game that means everything: Army and Navy get ready to clash for the 125th time in college football’s most unique rivalry. 
-        7;   South Korea’s parliament votes to impeach president.
-    The weather:
-        ; 33 degrees centigrade. 
-        ; Air Quality Index of 157 which is "Poor".
-        ; The humidity is 42% and .
-        ; The windspeed is 7 km/h.
 
-    So what can I do for you today?
-'''
-mp3_fp = BytesIO()
-
-print('Started converting')
-st_time = time.time()
-from queue import Queue
-from threading import Thread
 
 def play_audio(q:Queue):
     while not q.empty():
@@ -90,40 +66,3 @@ def play_audio(q:Queue):
         mp3_fp.seek(0)
         song = AudioSegment.from_file(mp3_fp, format="mp3")
         play(song)
-# started = False
-# for i , t in enumerate(text.split('\n')):
-#     if t.strip() != '':
-#         if not started:
-#             end_time = time.time()
-
-q = Queue(maxsize = 20)
-
-thread1 = Thread(target=play_audio, args =[q])
-
-
-print('Start converting....')
-completed = []
-for i, buff in enumerate(gTTS(text, lang = 'en', tld= 'co.uk').stream()):
-    print(f'Putting item {i}')
-    q.put(buff)
-    if i == 0:
-        end_time = time.time()
-        print(end_time - st_time)
-        thread1.start()
-    
-thread1.join()
-
-
-# song = AudioSegment.from_file(mp3_fp, format="mp3")
-
-
-# end_time = time.time()
-# play(song)
-
-
-
-
-print('Full cycle:\t',end_time - st_time)
-# print('pydub time:\t',end_time - mid_time)
-# print('gtts time:\t', mid_time - st_time)
-# tts.save('hello.mp3')
