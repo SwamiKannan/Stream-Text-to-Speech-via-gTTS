664720e1d666a621874b378c21d94923de075e83
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2024-12-18 10:49:24 +0530

    Update gtts_read_aloud.py
    Added test case / illustrative example

--- src/gtts_read_aloud.py
+++ src/gtts_read_aloud.py
@@ -56,3 +56,33 @@ def save_audio(text):
     end_time = time.time()
     return end_time - st_time
 
+if __name__ == "__main__":
+    text = '''
+    Good morning sir. The main updates today are as follows:
+    News:.
+        1;   Latest in the Middle East as US secretary of state meets with leaders in region.
+        2;   Meet the kids struggling to breathe in India’s choked capital.
+        3;   Buckingham Palace maid arrested after drunken brawl at work Christmas party.
+        4;   Indian Gukesh Dommaraju, 18, becomes the youngest ever chess world champion after Ding Liren blunder.
+        5;   Oldest human DNA reveals lost branch of the human family tree. 
+        6;   The game that means everything: Army and Navy get ready to clash for the 125th time in college football’s most unique rivalry. 
+        7;   South Korea’s parliament votes to impeach president.
+    The weather:
+        ; 33 degrees centigrade. 
+        ; Air Quality Index of 157 which is "Poor".
+        ; The humidity is 42% and .
+        ; The windspeed is 7 km/h.
+
+    So what can I do for you today?
+    '''
+    print('Playing sequential output......')
+    taken_time = process_sequential(text)
+    print(f'Sentence of {len(text)} characters transcribed and read. First text was processed in {taken_time} seconds\n\n\n')
+
+    print('Streaming output.....')
+    taken_time = stream_stt(text)
+    print(f'Sentence of {len(text)} characters transcribed and read. First text was processed in {taken_time} seconds\n\n\n')
+
+    print('Creating audio file to save...')
+    taken_time = save_audio(text)
+    print(f'Sentence of {len(text)} characters transcribed and read. File was processed and saved in {taken_time} seconds')
